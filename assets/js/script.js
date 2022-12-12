b3da10d3b531824b10e2394061f4206f406db17d
Marcin Kacperski <kacperski81@gmail.com>
2022-12-12 20:29:12 +0000

    added comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,14 @@ var shedule = JSON.parse(localStorage.getItem('shedule')) || {
     17: '',
 };
 
+// display current day on top of the page
 currentDay.text(moment().format('dddd MMMM Do'));
 
-// Apoitment added to localStorage 
+// create element to confirm that apoitment was added to shedule
 var confirmation = $('<p>Apoitment added to <span>localStorage</span> <i class="fas fa-check"></i></p>').addClass('confirmation');
 container.append(confirmation);
 
+// set background color based on hour of the day
 function textAreaBackground(num,el) {
     var currentHour = Number(moment().format('HH'));
     if(num < currentHour) {
@@ -30,11 +32,15 @@ function textAreaBackground(num,el) {
     }
 }
 
+// add time block to the website
 function addRow(hour) {
+    
     var row = $('<div>').addClass('row time-block');
     var hourOfDay = $('<div>').addClass('col-2 hour').text(time.format("HA"));
     var textarea = $('<textarea>').addClass('col-8').text(shedule[hour]);
+    
     textAreaBackground(hour,textarea);
+    
     var saveContainer = $('<div>').addClass('col-2 saveBtn');
     var saveIcon = $('<i>').addClass('fas fa-save');
     saveContainer.append(saveIcon);
@@ -42,18 +48,25 @@ function addRow(hour) {
     row.append(textarea);
     row.append(saveContainer);
     container.append(row);
+
+    // set event listener to the save icon
     saveContainer.on('click', 'i', function() {
         var toDo = textarea.val();
         shedule[hour] = toDo;
         confirmation.show('slow');
         $('html').animate({scrollTop: 0},500);
+        
+        // show confirmation
         setTimeout(function() {
             confirmation.hide('slow');
         }, 1500);
+
+        // save shedule to local storage
         localStorage.setItem('shedule',JSON.stringify(shedule));
     });
 }
 
+// display timeblock for each hour
 while(time.format('HH') < 18) {
     var hour = Number(time.format('HH'));
     addRow(hour);
